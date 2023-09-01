8496afe6f3c1b62bfbd154e118c67572388029ec
Sam Veldeman <sam.veldeman@gmail.com>
2023-09-01 14:43:18 +0200

    set fixed count var for q1

--- appy.py
+++ appy.py
@@ -8,18 +8,18 @@ conn = sqlite3.connect('./data/vivino.db')
 cursor = conn.cursor()
 
 # Query functions
-def query_highlight_10_wines(min_ratings, max_price, count):
+def query_highlight_10_wines(min_ratings, max_price):
     query = """
     SELECT v.id, v.name AS vintage_name, v.ratings_average, v.year, v.price_euros, v.ratings_count, w.url
     FROM vintages AS v
     JOIN wines AS w ON v.wine_id = w.id
     JOIN regions AS r ON w.region_id = r.id
     WHERE (v.ratings_average >= ? AND v.price_euros < ? AND v.price_discounted_from IS NULL)
-        OR (v.ratings_average >= ? AND v.price_euros BETWEEN 20 AND ? AND v.ratings_count > ? AND v.year != 'N.V.' AND v.price_discounted_from IS NULL)
+        OR (v.ratings_average >= ? AND v.price_euros BETWEEN 20 AND ? AND v.ratings_count > 1000 AND v.year != 'N.V.' AND v.price_discounted_from IS NULL)
     ORDER BY v.price_euros
     LIMIT 10;
     """
-    return conn.execute(query, (min_ratings, max_price, min_ratings, max_price, count)).fetchall()
+    return conn.execute(query, (min_ratings, max_price, min_ratings, max_price)).fetchall()
 
 def query_wines_with_taste_keywords():
     query = """
@@ -118,10 +118,9 @@ def main():
     
     if query_option == "Highlight 10 wines":
         # Add sliders
-        count = st.slider('Minimum Ratings Count', min_value=0.0, max_value=50000.0, step=100.0, value=1000.0)
         min_ratings = st.slider('Minimum Ratings Average', min_value=0.0, max_value=5.0, step=0.1, value=4.0)
         max_price = st.slider('Maximum Price (Euros)', min_value=0, max_value=100, step=1, value=50)
-        result = query_highlight_10_wines(min_ratings, max_price, count)
+        result = query_highlight_10_wines(min_ratings, max_price)
         # Convert the result to a DataFrame and create a new Plotly visualization
         columns = ['id', 'vintage_name', 'ratings_average', 'year', 'price_euros', 'ratings_count', 'url']
         df = pd.DataFrame(result, columns=columns)
