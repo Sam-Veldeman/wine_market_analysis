9f80c75996740cf0afcd3db9c99b612acc4ab853
Sam Veldeman <sam.veldeman@gmail.com>
2023-09-01 14:45:11 +0200

    set max price to 13k for slider

--- appy.py
+++ appy.py
@@ -119,7 +119,7 @@ def main():
     if query_option == "Highlight 10 wines":
         # Add sliders
         min_ratings = st.slider('Minimum Ratings Average', min_value=0.0, max_value=5.0, step=0.1, value=4.0)
-        max_price = st.slider('Maximum Price (Euros)', min_value=0, max_value=100, step=1, value=50)
+        max_price = st.slider('Maximum Price (Euros)', min_value=0, max_value=13000, step=1, value=50)
         result = query_highlight_10_wines(min_ratings, max_price)
         # Convert the result to a DataFrame and create a new Plotly visualization
         columns = ['id', 'vintage_name', 'ratings_average', 'year', 'price_euros', 'ratings_count', 'url']
